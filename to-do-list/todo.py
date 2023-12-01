a415961502cd3c3d29e0c5a51ae7b14a690bc97b
Crishna <chomankkrishna732003@gmail.com>
2023-12-01 17:06:24 +0530

    Added a Menu with empty command

--- to-do-list/todo.py
+++ to-do-list/todo.py
@@ -8,6 +8,35 @@ class todo:
         self.root.geometry("600x500+500+200")
 
 
+
+        #----------------------------- Menu ------------------------------------------
+        self.todo_menu = Menu(root)
+
+        # File Menu
+        self.m1 = Menu(self.todo_menu, tearoff=0)
+        self.m1.add_command(label="New", command=refresh)
+        self.m1.add_command(label="Uncompleted", command=uncompleted)
+        self.m1.add_command(label="Save", command=save)
+
+        self.todo_menu.add_cascade(label="File", menu=self.m1)
+
+        # Other Menu
+        self.todo_menu.add_command(label="Download", command=download)
+
+        self.root.config(menu = self.todo_menu)
+
+
+
+# Menu Functions 
+def refresh():
+    pass
+def save():
+    pass
+def uncompleted():
+    pass
+def download():
+    pass
+
 def main():
     root = Tk()
     win = todo(root)
